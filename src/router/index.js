b32142f923ebbd7d8dd604a98410a525f8afe449
longer <369436396@qq.com>
2018-03-31 16:48:49 +0800

    新增首页。

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,20 @@ import Vue from "vue";
 import Router from "vue-router";
 Vue.use(Router);
 
-const test = r => require.ensure([], () => r(require("views/test")), "test");
-// 游戏中心
-const gameCenter = r => require.ensure([], () => r(require("views/game-center")), "game");
-// 游戏详情
-const gameDetail = r => require.ensure([], () => r(require("views/game-detail")), "game");
+const index = r => require.ensure([], () => r(require("views/index")), "index");
 
 export default new Router
 ({
     routes:
     [
+        {
+            path: "",
+            redirect: "/index"
+        },
+        {
+            path: "/index",
+            name: "index",
+            component: index
+        }
     ]
 })
---
+++ src/views/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div><h1>滴水救助</h1></div>
+</template>
